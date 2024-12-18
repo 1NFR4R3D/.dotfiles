64b8f2851039b0723ffc547f8db20c21a5e210bf
Kanishkavikram Purohit <kpurohit@tewke.com>
2024-12-18 16:32:50 +0000

    Customise prompt colours for context(user@hostname)

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -966,14 +966,17 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=1
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=0
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=015
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=001
   # Context color in SSH without privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=3
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_BACKGROUND=0
+  typeset -g POWERLEVEL9K_CONTEXT_REMOTE_FOREGROUND=000
+  typeset -g POWERLEVEL9K_CONTEXT_REMOTE_BACKGROUND=190
+  # Context color in SSH without privileges.
+  typeset -g POWERLEVEL9K_CONTEXT_REMOTE_SUDO_FOREGROUND=001
+  typeset -g POWERLEVEL9K_CONTEXT_REMOTE_SUDO_BACKGROUND=190
   # Default context color (no privileges, no SSH).
-  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=3
-  typeset -g POWERLEVEL9K_CONTEXT_BACKGROUND=0
+  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=015
+  typeset -g POWERLEVEL9K_CONTEXT_BACKGROUND=021
 
   # Context format when running with privileges: user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%n@%m'
