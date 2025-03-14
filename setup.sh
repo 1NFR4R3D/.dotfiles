55a066fca2da00fce699441a8d194a03ba39d41b
Kanishkavikram Purohit <kpurohit@tewke.com>
2025-03-14 14:47:44 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 echo "TODO: Write this script to fulfill all requirements for the stows"
 
+echo "Install some essentials(assuming apt)"
+sudo apt install git lolcat zsh stow tmux vim 
+
 echo "Set git global gitignore"
 git config --global core.excludesfile ~/.gitignore_global
